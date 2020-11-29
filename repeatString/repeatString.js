210cfb6b7ad40ce27a8a756acb841f53ad881b88
MSoup <19704779+MSoup@users.noreply.github.com>
2020-11-29 21:48:18 +0900

    finished TOP first set of problems

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,4 +1,7 @@
 const repeatString = function(word, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
     let ans = "";
     for (let i = 0; i < num; i++) {
         ans += word;
--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(F) {
+  return Math.round(((F - 32) * (5/9)) * 10) / 10
 }
 
-const ctof = function() {
-
+const ctof = function(C) {
+  return Math.round((C * 1.8 + 32) * 10) / 10
 }
 
 module.exports = {
