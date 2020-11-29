1112316aadd22b2b473c5de0043c17e64d4715a8
MSoup <19704779+MSoup@users.noreply.github.com>
2020-11-29 21:19:30 +0900

    solved

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof start !== "number" | typeof end !== "number" | start < 0 | end < 0) {
+        return "ERROR"
+    }
+    const lower = Math.min(start, end)
+    const higher = Math.max(start, end)
 
+    let ans = 0;
+    for (let i = lower; i <= higher; i++) {
+        ans += i;
+    }
+    return ans;
 }
 
 module.exports = sumAll
+
