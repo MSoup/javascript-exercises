ccc7f33fbef3b3fedd7ecd39500b341dd2d65e28
MSoup <19704779+MSoup@users.noreply.github.com>
2020-11-29 15:35:27 +0900

    exercises

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    const ans = arr.filter((el) =>
+        (args.indexOf(el) == -1)
+    );
 
+    return ans;
+    
 }
+
 
 module.exports = removeFromArray
--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let ans = "";
+    for (let i = 0; i < num; i++) {
+        ans += word;
+    }
+    return ans;
 }
 
 module.exports = repeatString
