460e36e8a7e196b6670ec37fed2895c418569b9e
MSoup <19704779+MSoup@users.noreply.github.com>
2020-12-16 06:48:24 +0900

    finished 2nd set of problems TOP

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,42 @@
-const caesar = function() {
+const caesar = function (phrase, shift) {
+  let char;
+  let newPhrase = "";
+  let actualShift = shift % 26;
+  let shifted;
 
-}
+  for (let i = 0; i < phrase.length; i++) {
+    char = phrase[i];
 
-module.exports = caesar
+    if (isUpper(char)) {
+      // shift, then check boundaries
+      shifted = checkBoundaries(char.charCodeAt(0) - 65 + actualShift);
+      newPhrase += String.fromCharCode(shifted + 65);
+    } else if (isLower(char)) {
+      shifted = checkBoundaries(char.charCodeAt(0) - 97 + actualShift);
+      newPhrase += String.fromCharCode(shifted + 97);
+    } else {
+      newPhrase += char;
+    }
+  }
+  return newPhrase;
+};
+
+const checkBoundaries = function (num) {
+  if (num < 0) {
+    return num + 26;
+  } else if (num > 0) {
+    return num % 26;
+  } else {
+    return num;
+  }
+};
+
+const isUpper = function (char) {
+  return char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 65 + 26;
+};
+
+const isLower = function (char) {
+  return char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 97 + 26;
+};
+
+module.exports = caesar;
